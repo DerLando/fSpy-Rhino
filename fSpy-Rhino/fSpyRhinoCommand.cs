3d596c1dac89bdb5dc338a05fc422e52842d370d
Lando <lando.schumpich@gmail.com>
2019-08-05 19:30:20 +0200

    Projektdateien hinzufügen.

---
+++ fSpy-Rhino/fSpyRhinoCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace fSpy_Rhino
+{
+    public class fSpyRhinoCommand : Command
+    {
+        public fSpyRhinoCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static fSpyRhinoCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "fSpyRhinoCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // Usually commands in import plug-ins are used to modify settings and behavior.
+            // The import work itself is performed by the fSpyRhinoPlugIn class.
+
+            return Result.Success;
+        }
+    }
+}
